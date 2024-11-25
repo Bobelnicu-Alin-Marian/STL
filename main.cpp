b38a209d972a569704a781ba9369f84418543c70
Alin-Marian Bobelnicu <marian9bobelnicu@gmail.com>
2024-11-25 23:16:22 +0200

    #commit1.1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+struct Problem {
+    string id_problem;
+    string speciality;
+};
+struct Doctor {
+    string id_doctor;
+    string speciality;
+};
 
 int main()
 {
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
-    string name, speciality;
-    
     inFile >> no_problems;
-
-    for (int i = 0; i < no_problems; i++)
+    vector<Problem>p(no_problems);
+    for (Problem &pb : p)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >>pb.id_problem;
+        inFile >> pb.speciality;
     }
+    
 
     inFile >> no_doctors;
-
-    for (int i = 0; i < no_doctors; i++)
+    vector<Doctor>d(no_doctors);
+    for(Doctor &dr : d)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> dr.id_doctor;
+        inFile >> dr.speciality;
     }
-
+    bool ok;
+    for (const Problem& pb : p)
+    {
+        ok = false;
+        for (const Doctor& dr : d)
+        {
+            if (pb.speciality == dr.speciality)
+                ok = true;
+        }
+        if(ok)
+            cout << pb.id_problem << " " << "Acceptat" << '\n';
+        else
+            cout << pb.id_problem << " " << "Respins" << '\n';
+    }
+    inFile.close();
     return 0;
 }
