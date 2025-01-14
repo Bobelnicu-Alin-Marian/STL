f5c1ed64e9f809cae2975030ae15c702a1295008
Alin-Marian Bobelnicu <marian9bobelnicu@gmail.com>
2025-01-14 13:20:20 +0200

    //pb1-stl4

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
 
 using namespace std;
 struct Problem {
     string id_problem;
     string speciality;
+    int ora_sosire;
+    int priority;
+    int durata;
+    bool operator> (const Problem& other) const {
+        return this->priority > other.priority;
+    }
 };
 struct Doctor {
     string id_doctor;
-    string speciality;
-    bool visited = false;
+    int nr_spec;
+    vector<string> specialities;
+    int available_time = 9;
+
 };
 
 int main()
@@ -26,29 +37,41 @@ int main()
     {
         inFile >>pb.id_problem;
         inFile >> pb.speciality;
+        //inFile >> pb.ora_sosire;
+        inFile >> pb.durata;
+        inFile >> pb.priority;
     }
-    
+    multiset<Problem, greater<Problem>> sorted_problems(p.begin(), p.end());
 
     inFile >> no_doctors;
     vector<Doctor>d(no_doctors);
-    for(Doctor &dr : d)
-    {
+    for(auto &dr : d){
         inFile >> dr.id_doctor;
-        inFile >> dr.speciality;
+        inFile >> dr.nr_spec;
+        for (int i = 1; i <= dr.nr_spec; i++) {
+            string spec;
+            inFile >> spec;
+            dr.specialities.emplace_back(spec);
+        }
     }
-    for (const Problem& pb : p)
-    {
-       
-            auto ans = find_if(d.begin(), d.end(), [=](Doctor& dr) {
-                return dr.visited == false && pb.speciality == dr.speciality;
-                });
-            if (ans != d.end()) {
-                ans->visited = true;
-                cout << ans->id_doctor<<" "<< pb.id_problem << '\n';
+    
+    for (const Problem& pb : sorted_problems) {
+        bool visited = false;
+        for (Doctor& dr : d) {
+            if (find(dr.specialities.begin(), dr.specialities.end(), pb.speciality) != dr.specialities.end() && dr.available_time <= 17) {
+                cout << dr.id_doctor << " " << pb.id_problem << '\n';
+                dr.available_time += pb.durata;
+                visited = true;
+                break;
             }
+        }
 
-     
+        if (!visited) {
+            cout << "Nu exista doctor disponibil pentru problema " << pb.id_problem << endl;
+        }
     }
+
+
     inFile.close();
     return 0;
 }
